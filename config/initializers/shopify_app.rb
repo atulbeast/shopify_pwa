a46ad87be63453b51c67bf07f8b0408bd2934f3a
atulbishtZig <atul@ziggletech.com>
2019-01-24 15:15:45 +0530

    credential initialized from env file

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
-  config.api_key = "7ff852a9c817b319c1705f2a32941e00"
-  config.secret = "06fc4de59787c55d49360fa783399498"
+  config.api_key = ENV['SHOPIFY_API_KEY']
+  config.secret = ENV['SHOPIFY_SECRET_KEY']
   config.scope = "read_orders,read_products,write_products,read_script_tags, write_script_tags,read_content, write_content" # Consult this page for more scope options:
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = true
