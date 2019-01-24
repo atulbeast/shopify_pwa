eeb2c7362f0d1fa97c6d9c29266936937c753b45
atulbishtZig <atul@ziggletech.com>
2019-01-24 14:50:13 +0530

    Generate shopify_app

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,10 @@
+ShopifyApp.configure do |config|
+  config.application_name = "My Shopify App"
+  config.api_key = "7ff852a9c817b319c1705f2a32941e00"
+  config.secret = "06fc4de59787c55d49360fa783399498"
+  config.scope = "read_products" # Consult this page for more scope options:
+                                 # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
+  config.embedded_app = true
+  config.after_authenticate_job = false
+  config.session_repository = Shop
+end
